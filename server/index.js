dc0c1b20f061c21a1db36df59ecb618566a1b794
Izzyosman <137222951+Izzyosman@users.noreply.github.com>
2023-08-09 21:23:20 -0500

    second commit

--- server/index.js
+++ server/index.js
@@ -1,11 +1,34 @@
-const express = require("express");
-const cors = require("cors");
-const bodyParser = require("body-parser");
-
-const app = express();
+const express = require('express');
+const cors = require('cors');
 
 app.use(cors());
 app.use(express.json());
+
+let tasks = [];
+
+app.get('/api/tasks', (req, res) => {
+  res.json(tasks);
+});
+
+app.post('/api/tasks', (req, res) => {
+  const newTask = req.body;
+  tasks.push(newTask);
+  res.status(201).json(newTask);
+});
+
+app.put('/api/tasks/:id', (req, res) => {
+  const taskId = req.params.id;
+  const updatedTask = req.body;
+  tasks = tasks.map(task => (task.id === taskId ? updatedTask : task));
+  res.json(updatedTask);
+});
+
+app.delete('/api/tasks/:id', (req, res) => {
+  const taskId = req.params.id;
+  tasks = tasks.filter(task => task.id !== taskId);
+  res.sendStatus(204);
+});
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
